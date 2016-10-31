0cc2481c4e46a5d977704382eb59770dcb14a5d8
YongPilMoon <m71661@gmail.com>
2016-10-31 15:21:52 +0900

    Update diary_calendar url

--- diarySitePrj/diary/urls.py
+++ diarySitePrj/diary/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import url
 from diary import views
 urlpatterns = [
     url(r'^add/$', views.diary_add, name='diary_add'),
+    url(r'^calendar/$', views.diary_calendar, name='diary_calendars'),
+
 ]
