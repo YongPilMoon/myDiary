2ad3f2d1046fdb0901c76732786659dd38608bce
YongPilMoon <m71661@gmail.com>
2016-11-04 19:01:21 +0900

    Add Examplemodel to admin and makemigrations

--- diarySitePrj/diary/admin.py
+++ diarySitePrj/diary/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Diary
+from .models import Diary, ExampleModel
 # Register your models here.
 admin.site.register(Diary)
+admin.site.register(ExampleModel)
