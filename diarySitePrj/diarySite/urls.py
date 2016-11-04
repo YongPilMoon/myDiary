cc944f3e543dede92d84c6a22a0bda5cb28028a7
YongPilMoon <m71661@gmail.com>
2016-11-04 19:03:36 +0900

    Add photo url

--- diarySitePrj/diarySite/urls.py
+++ diarySitePrj/diarySite/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^diary/', include('diary.urls', namespace='diary')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
