99c0e96e0ed0e46ecaec69a67b68a38ae4d02a23
YongPilMoon <m71661@gmail.com>
2016-11-20 18:42:51 +0900

    User models customizing
    id를 username대신 email로 사용하기 위해 DiaryUser model을 customizing 하였다.

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,14 +1,57 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
-class DiaryUserManager(UserManager):
-    pass
+class DiaryUserManager(BaseUserManager):
+    def create_user(
+            self,
+            email,
+            nickname,
+            password=None,
+            ):
+        user = self.model(
+            email=email,
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(
+            self,
+            email,
+            nickname,
+            password=None,
+            ):
+        user = self.model(
+            email=email,
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
 
 
-class DiaryUser(AbstractUser):
+class DiaryUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    nickname = models.CharField(max_length=24)
+    is_staff = models.BooleanField(default=False)
 
     objects = DiaryUserManager()
 
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['nickname']
+
     def __str__(self):
         return self.username
+
+    def get_full_name(self):
+        return self.nickname
+
+    def get_short_name(self):
+        return self.nickname
+
