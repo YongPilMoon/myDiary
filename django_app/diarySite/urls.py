cd0c8aabc762103926e9356ea4f937fbc950fe38
YongPilMoon <m71661@gmail.com>
2016-11-20 17:01:31 +0900

    Add index page and change design
    기존 main페이지였던 login페이지를 대신해 index페이지를 추가하였다. 메인 페이지에 사이트 소개 및 signup, login링크를 추가할 예정이다.또한 bootstrap의 customizing에  한계를 느껴 순수 css를 이용하여 다시 design하였다.

--- django_app/diarySite/urls.py
+++ django_app/diarySite/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^diary/', include('diary.urls', namespace='diary')),
     url(r'^admin/', admin.site.urls),
     url(r'^member/', include('member.urls', namespace='member')),
+    url(r'^index/$', TemplateView.as_view(template_name='index.html'), name='index')
 ]
 
 if settings.DEBUG:
